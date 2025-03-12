c09f2fe23a12cb3cfee80bb63153fd07c10c7766
Sebastian <epicsauce2013@gmail.com>
2025-03-11 17:57:35 -0700

    FIGURED OUT THE ISSUE WITH THE UPGRADING, i mean i figured my bad loop for a function that wants a list would bite me in the future, and well it was basically doing some werid stuff when you misuse it so now cards on level up get stored in list then sent all in one batch

--- application/MiddleEnd/MasteryCardGraderWCustomData.py
+++ application/MiddleEnd/MasteryCardGraderWCustomData.py
@@ -8,20 +8,6 @@ from anki.collection import Collection
 from anki.decks import DeckConfigDict
 from aqt.utils import tooltip
 
-
-
-from anki.collection import Collection, OpChanges
-from anki.decks import DeckConfigDict
-from aqt import qconnect
-from aqt.browser import Browser
-from aqt.operations import CollectionOp
-from aqt.operations import ResultWithChanges
-from aqt.qt import QKeySequence
-from aqt.utils import tooltip
-import functools
-
-
-
 class MasteryUpdate(Enum):
     DECREASE = -1
     STAY = 0
@@ -45,34 +31,17 @@ class CardState(Enum):
     REVIEW = CARD_TYPE_REV
     RELEARNING = CARD_TYPE_RELEARNING
     
+from aqt.operations.collection import CollectionOp
 
-
-
-# TODO see if you need to update_card with every set or if it works to update_cards after the for loop
 class MasterySharedUtils:
     def set_mastery_data_levels(self, note_type_id_from_anki: int):
         self.MasteryDataLevels = masteryDatahandler.get_all_rep_count_tags(str(note_type_id_from_anki))
-        
-        
-    # def with_undo_entry(undo_msg: str):
-    #     def decorator(function: Callable):
-    #         @functools.wraps(function)
-    #         def wrapper(self,*args, **kwargs) -> ResultWithChanges:
-    #             pos = mw.col.add_custom_undo_entry(undo_msg)
-    #             function(mw.col, *args, **kwargs)
-    #             return mw.col.merge_undo_entries(pos)
-
-    #         return wrapper
-
-    #     return decorator
     
     def set_success_count_data(self, card: Card, rep_count_card, rep_count_note):
         data = json.loads(card.custom_data or '{}')
-        print(f"BDATA: {data} [CC: {rep_count_card}|NC:{rep_count_note}]")
         data['cardsct'] = rep_count_card
         data['notesct'] = rep_count_note
         card.custom_data = json.dumps(data)
-        print(f"ADATA: {data} [CC: {rep_count_card}|NC:{rep_count_note}]")
         mw.col.update_card(card)
     
     def set_note_success_count(self, card: Card, note_rep_count):
@@ -95,31 +64,59 @@ class MasterySharedUtils:
         data = json.loads(card.custom_data or '{}')
         return data.get('cardsct', 0)
 
-    def suspend_unsuspend_a_card(self, note:Note, card: Card, success_count):
-        template_name = card.template()['name']
-        # print(f"TMPL: {template_name} | NID: {note.id} | NTID: {note.note_type()['id']}")
-        
-        ntID = note.note_type()['id']
-        
-        min_level = masteryDatahandler.get_note_type_template_min_level(ntID, template_name)
-        max_level = masteryDatahandler.get_note_type_template_max_level(ntID, template_name)
-        print(f" {min_level} <= {success_count} <= {max_level}")
-        if min_level <= success_count <= max_level:
-            mw.col.sched.unsuspend_cards([card.id])
-            # mw.col.update_card(card)
-            print(f"unlocked:{template_name}")
-
-        else:
-            mw.col.sched.suspend_cards([card.id])
-            # mw.col.update_card(card)
-            print(f"locked:{template_name}")
-
-
+    # def suspend_unsuspend_a_card(self, note:Note, card: Card, success_count):
+    #     template_name = card.template()['name']
+    #     ntID = note.note_type()['id']
+    #     min_level = masteryDatahandler.get_note_type_template_min_level(ntID, template_name)
+    #     max_level = masteryDatahandler.get_note_type_template_max_level(ntID, template_name)
+    #     if min_level <= success_count <= max_level:
+    #         if card.queue == -1:
+    #             pass
+    #             # mw.col.sched.unsuspend_cards([card.id])
+    #             # print(f"UNLOCKed ({card.queue}): [{template_name}], [cardID:{card.id}]: {min_level} <= {success_count} <= {max_level}")
+    #             # card.load()
+    #             # mw.col.update_card(card)
+    #     else:
+    #         if card.queue != -1:
+    #             mw.col.sched.suspend_cards([card.id])
+    #             print(f"LOCKed ({card.queue}): [{template_name}], [cardID:{card.id}]: {min_level} <= {success_count} <= {max_level}")
+    #             mw.col.update_card(card)
+    #             card.load()
+    #     user = input("PAUSE")
 
     def suspend_unsuspend_cards(self, note:Note, success_count):
+        cards_to_suspend = []
+        cards_to_unsuspend = []
         for card in note.cards():
-            self.suspend_unsuspend_a_card(note, card, success_count)
-        mw.col.update_cards(note.cards())
+            template_name = card.template()['name']
+            ntID = note.note_type()['id']
+            min_level = masteryDatahandler.get_note_type_template_min_level(ntID, template_name)
+            max_level = masteryDatahandler.get_note_type_template_max_level(ntID, template_name)
+            if min_level <= success_count <= max_level:
+                if card.queue == -1:
+                    cards_to_unsuspend.append(card.id)
+            else:
+                if card.queue != -1:
+                    cards_to_suspend.append(card.id)
+        print(f"UNLK: {cards_to_unsuspend}\nLOCK: {cards_to_suspend}")
+        user1 = input("PAUSEBEFORE")
+        mw.col.sched.suspend_cards(cards_to_suspend)
+        user2 = input("PAUSEMID")
+        mw.col.sched.unsuspend_cards(cards_to_unsuspend)
+        user3 = input("PAUSELAST")
+            
+            
+            
+            
+            
+            # self.suspend_unsuspend_a_card(note, card, success_count)
+        # mw.col.save()
+        # mw.col.sched.unsuspendCards()
+        # mw.reset()
+        # CollectionOp(parent=mw, op=lambda col: None).run_in_background()  # Properly refresh UI
+
+        
+        # mw.col.update_cards(note.cards())
 
 
 
@@ -160,7 +157,6 @@ class mastery_card_add(MasterySharedUtils):
 
         reps_left = len(group_conf["new"]["delays"])
 
-        # print("delays:", group_conf["new"]["delays"], "reps left:", reps_left)
         return reps_left * 1000 + reps_left
     
     def put_in_learning(self, card: Card) -> None:
@@ -176,10 +172,9 @@ class mastery_card_add(MasterySharedUtils):
 
     def put_in_review(self, card: Card) -> None:
         cfg = mw.col.decks.config_dict_for_deck_id(self.current_deck_id(card))
-        # print_nested_dict(cfg)
         card.type = 2
         card.queue = 2
-        card.ivl = cfg["new"]["ints"][0]  # Or cfg["new"]["easyIv"] if you're feeling generous
+        card.ivl = cfg["new"]["ints"][0]
         card.due = mw.col.sched.today + card.ivl
         card.reps = 0
         card.lapses = 0
@@ -222,57 +217,40 @@ masteryCardAdder = mastery_card_add()
 class mastery_card_grader(MasterySharedUtils):
     def __init__(self):
         self.MasteryDataLevels = None 
-
-    # def sync_note_success_count_to_siblings(self, note: Note, new_success_count):
-    #     for card in note.cards():
-    #         self.set_note_success_count(card, new_success_count)
     
-    
-    def update_success_count_data(self, active_card:Card, new_card_count, note, new_note_count):
+    def update_success_count_data(self, active_card:Card, new_card_count, note:Note, new_note_count):
         for card in note.cards():
-            print(f"card:{card.id} == {active_card.id}:{card.id == active_card.id}")
             if card.id == active_card.id:
                 self.set_success_count_data(card, new_card_count, new_note_count)
             else:
                 self.set_note_success_count(card, new_note_count)
-
-        # mw.col.update_cards(note.cards())
         
     def adjust_success_count(self, ease_button, current_count, min_count, max_count):
-        # print(f"{ease_button} == {AnkiButton.AGAIN.value} and {current_count} > {min_count}")
         result = current_count
         status = MasteryUpdate.STAY
         if ease_button == AnkiButton.AGAIN.value and current_count > min_count:
             result = max(current_count - 1, min_count)
             status = MasteryUpdate.DECREASE
-            print(f"AGAIN {result} - [{current_count} - 1 ={current_count - 1}, {min_count}]")
         elif ease_button == AnkiButton.GOOD.value and current_count < max_count:
             result = min(current_count + 1, max_count)
             status = MasteryUpdate.INCREASE
-            print(f"GOOD {result} - [{current_count} + 1={current_count + 1}, {max_count}]")
         return result, status
         
     def get_current_note_success(self, note:Note, card:Card, ease_button):
-        print("GETTING NOTE SUCCESS COUT")
         ntID = note.note_type()['id']
         min_note = masteryDatahandler.get_start_number(ntID)
         max_note = len(masteryDatahandler.get_all_rep_count_tags(ntID)) - 1
         current_note_count = self.get_note_success_count(card)
-        print(f"getting note count:{current_note_count}")
         new_note_count, update_status = self.adjust_success_count(ease_button, current_note_count, min_note, max_note)
-        print(f"After getting new note count:{new_note_count} | status:{update_status}")
         return current_note_count, new_note_count, update_status
         
     def get_current_card_success(self, note:Note, card:Card, ease_button):
-        print("GETTING CARD SUCCESS COUT")
         ntID = note.note_type()['id']
         template_name = card.template()['name']
         min_card = masteryDatahandler.get_note_type_template_min_level(ntID, template_name)
         max_card = masteryDatahandler.get_note_type_template_max_level(ntID, template_name)
         current_card_count = self.get_card_success_count(card)
-        print(f"getting card count:{current_card_count}")
         new_card_count, update_status = self.adjust_success_count(ease_button, current_card_count, min_card, max_card)
-        print(f"After getting new card count:{new_card_count} | status:{update_status}")
         return current_card_count, new_card_count, update_status
     
     def level(self, success_count, note: Note):
@@ -282,7 +260,6 @@ class mastery_card_grader(MasterySharedUtils):
             min_level = masteryDatahandler.get_note_type_template_min_level(ntID, template_name)
             max_level = masteryDatahandler.get_note_type_template_max_level(ntID, template_name)
             if min_level <= success_count <= max_level:
-                print(f"{template_name} | MIN:{min_level} <= SUCCT:{success_count} <= MAX:{max_level}")
                 return level
         return -1
     
@@ -297,18 +274,17 @@ class mastery_card_grader(MasterySharedUtils):
             self.suspend_unsuspend_cards(note, new_count)
 
             if not active_card.reps:
-                # print("LEVEL UP")
-                # print(f"CHANGED DUE = [B: {card.due} | A:{mw.col.sched.today + 1}]")
                 active_card.due = mw.col.sched.today + 1
-                mw.col.update_card(active_card)
+                # mw.col.update_card(active_card)
                 status = LevelUpStatus.LEVEL_CHANGED_REPS_ZERO
             else:
-                # print("PROMPTED")
                 status = LevelUpStatus.LEVEL_CHANGED_REPS_NOT_ZERO
         return status
     
     
     def handle_message_for_level_up(self, note: Note, level_up_status, update_status, ease_button, old_count, new_count):
+        prev_card = note.cards()[self.level(old_count, note)]
+        prev_card_name = prev_card.template()['name']
         card = note.cards()[self.level(new_count, note)]
         name = card.template()['name']
         btn = AnkiButton(ease_button)
@@ -316,10 +292,10 @@ class mastery_card_grader(MasterySharedUtils):
         arrow = f"[{old_count} → {new_count}]"
         
         def notify(msg, period=6000):
-            print(msg)
+            
+            print(f"\n====================\n[msg:{msg}]\nBTNPUSHED:{btn}\nMASTERYUPATE:{update_status}\n\t- OLDCOUNT:{old_count}\n\t- NEWCOUNT:{new_count}\nLVLSTATUS:{level_up_status}\n\t- PREVCARD:{prev_card_name}\n\t- NEWCARD:{name}\n====================\n")
             tooltip(msg, period=period)
 
-        # Define condition-message mappings
         conditions_to_messages = {
             (LevelUpStatus.NO_LEVEL_UP, MasteryUpdate.STAY, AnkiButton.AGAIN): f"Reps min reached: {same} | {name}",
             (LevelUpStatus.NO_LEVEL_UP, MasteryUpdate.STAY, AnkiButton.GOOD): f"Reps max reached: {same} | {name}",
@@ -329,8 +305,6 @@ class mastery_card_grader(MasterySharedUtils):
             (LevelUpStatus.LEVEL_CHANGED_REPS_NOT_ZERO, MasteryUpdate.INCREASE, AnkiButton.GOOD): f"Reps increased: {arrow} | Level ⬆️ {name}",
             (LevelUpStatus.LEVEL_CHANGED_REPS_NOT_ZERO, MasteryUpdate.DECREASE, AnkiButton.AGAIN): f"Reps decreased: {arrow} | Level ⬇️ {name}",
         }
-
-        # Try to get the message based on the combination of (level_up_status, update_status, btn)
         msg = conditions_to_messages.get((level_up_status, update_status, btn))
 
         if msg:
@@ -339,9 +313,6 @@ class mastery_card_grader(MasterySharedUtils):
             notify(f"UNKNOWN-CONDTIONS: {level_up_status}, {update_status}, {btn}, reps[B:{old_count} A:{new_count}]", period=15000)
 
     def on_card_grade(self, reviewer:Reviewer=None, active_card:Card=None, ease_button=None):
-        
-        # pos = mw.col.add_custom_undo_entry("CARD MASTERY GRADE UNDO")
-        # print(f"LASTSTEP:{mw.col.undo_status().last_step} - 2")
         last = mw.col.undo_status().last_step
         note = active_card.note()
         ntID = note.note_type()['id']
@@ -349,25 +320,9 @@ class mastery_card_grader(MasterySharedUtils):
         old_note_count, new_note_count, overall_update_status = self.get_current_note_success(note, active_card, ease_button)
         current_card_count, new_card_count, card_update_status = self.get_current_card_success(note, active_card, ease_button)
         level_up_status = self.handle_sched_and_sus_on_level_up(old_note_count, new_note_count, note)
-        # mw.col.update_cards(note.cards())
         self.update_success_count_data(active_card, new_card_count, note, new_note_count)
-            # user0 = input("PAUSED AFTER sus/unsus")
-        # self.set_card_success_count(card, new_card_count)
-            # user2 = input("PAUSED AFTER  card cucess count")
-        # self.sync_note_success_count_to_siblings(note, new_note_count)
-            # user3 = input("PAUSED AFTER  note sibling sync")
-        # XXX sus/unsus was here before just as a reminder
         self.handle_message_for_level_up(note, level_up_status, overall_update_status, ease_button, old_note_count, new_note_count)
-            # user4 = input("PPAUSED AFTER  sent message")
-        
-            # user4 = input("PPAUSED AFTER  update message")
-        # print(mw.col.undo_status().last_step)
-        # mw.col.merge_undo_entries(pos)
         mw.col.merge_undo_entries(last)
 
-        
-        
-        
-        
 
 masteryCardGrader = mastery_card_grader()
